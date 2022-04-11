568239c5baa7066ffe65aafc3ff9e462ddb385dd
bft7658 <bft7658@gmail.com>
2022-04-11 22:40:38 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -96,6 +96,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch((error) => console.log(error));
 })
 
+// 刪除特定餐廳資料
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(item =>
