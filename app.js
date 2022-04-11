28eca4caba944a7a623d89ab28f48804ff9011f5
bft7658 <bft7658@gmail.com>
2022-04-11 10:46:36 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 // 使用 require 載入 Express
 const express = require('express')
+// 載入 mongoose
+const mongoose = require('mongoose')
+
+
+
+
+
 const app = express()
 const port = 3000
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('Error connecting')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('Connected to MongoDB')
+})
+
 
 // 在 Express 中使用樣版引擎
 const exphbs = require('express-handlebars')
@@ -34,5 +55,5 @@ app.get('/search', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+  console.log(`This website is running on http://localhost:${port}`)
 })
---
+++ models/restaurant.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+// 把我們想要的資料結構當成參數傳給 new Schema()
+const restaurantSchema = new Schema({
+  id: {
+    type: Number,
+    required: false
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+})
+// 透過 module.exports 把這個 schema 輸出。匯出的時候我們把這份 schema 命名為 Restaurant，以後在其他的檔案直接使用 Restaurant 就可以操作和「餐廳」有關的資料了！
+module.exports = mongoose.model('Restaurant', restaurantSchema)
