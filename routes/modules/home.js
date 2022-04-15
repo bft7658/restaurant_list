3f8951910a8d121ff8bcc3c8b7c5322487da8733
bft7658 <bft7658@gmail.com>
2022-04-15 20:14:56 +0800

    feat: modify sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,17 +5,21 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 // 瀏覽全部餐廳
 router.get('/', (req, res) => {
+  const sort = req.query.sort || 'name'
   // 從資料庫找出資料
   Restaurant.find()
     // 撈資料以後想用 res.render()，要先用 .lean() 來處理
     .lean()
-    .sort({ _id: 'asc' })
+    // .sort({ _id: 'asc' })
+    .sort(sort)
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
 
 // 搜尋餐廳
 router.get('/search', (req, res) => {
+  // 用 query string 方式
+  const sort = req.query.sort || 'name'
   const keyword = req.query.keyword
   Restaurant.find({
     $or: [
@@ -24,36 +28,40 @@ router.get('/search', (req, res) => {
     ]
   })
     .lean()
+    .sort(sort)
     .then((restaurants) => {
       res.render('index', { restaurants, keyword });
     })
-    // Restaurant.find()
-    //   .lean()
-    //   .then(restaurants => {
-    //     const filterRestaurants = restaurants.filter(data =>
-    //       data.name.toLowerCase().includes(keyword) ||
-    //       data.name_en.toLowerCase().includes(keyword) ||
-    //       data.category.toLowerCase().includes(keyword)
-    //     )
-    //     res.render('index', { restaurants: filterRestaurants, keyword })
-    //   })
     .catch(error => console.log(error))
+
+  // Restaurant.find()
+  //   .lean()
+  //   .then(restaurants => {
+  //     const filterRestaurants = restaurants.filter(data =>
+  //       data.name.toLowerCase().includes(keyword) ||
+  //       data.name_en.toLowerCase().includes(keyword) ||
+  //       data.category.toLowerCase().includes(keyword)
+  //     )
+  //     res.render('index', { restaurants: filterRestaurants, keyword })
+  //   })
+  //     .catch(error => console.log(error))
 })
 
-router.get('/:sort', (req, res) => {
-  const selectedSort = req.params.sort
-  const sortObj = {
-    '0': { name_en: 'asc' },
-    '1': { name_en: 'desc' },
-    '2': { rating: 'desc' },
-    '3': { rating: 'asc' }
-  }
-  Restaurant.find()
-    .lean()
-    .sort(sortObj[selectedSort])
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.error(error))
-})
+// 用 params 方式
+// router.get('/:sort', (req, res) => {
+//   const sort = req.params.sort
+//   const sortObj = {
+//     '0': { name_en: 'asc' },
+//     '1': { name_en: 'desc' },
+//     '2': { rating: 'desc' },
+//     '3': { rating: 'asc' }
+//   }
+//   Restaurant.find()
+//     .lean()
+//     .sort(sortObj[sort])
+//     .then(restaurants => res.render('index', { restaurants }))
+//     .catch(error => console.error(error))
+// })
 
 // 匯出路由模組
 module.exports = router
