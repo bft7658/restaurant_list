9ffe93bed3e65357509026200428881b70b46a4c
bft7658 <bft7658@gmail.com>
2022-05-11 10:16:52 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 const home = require('./modules/home')
 // 引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 // 將網址結構符合的字串導向對應的模組 
 router.use('/' || '/search', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
