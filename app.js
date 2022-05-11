bc5823cd007dee1fe9c80714065c9a150553a511
bft7658 <bft7658@gmail.com>
2022-05-11 23:47:10 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // 使用 require 載入 Express
 const express = require('express')
-// 引用 body-parser
-const bodyParser = require('body-parser')
 // 載入 session
 const session = require('express-session')
+// 引用 body-parser
+const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 // 引用路由器
 const routes = require('./routes')
 // 載入 passport 設定檔，要寫在 express-session 以後
@@ -34,9 +35,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 // 將 request 導入路由器
--- config/passport.js
+++ config/passport.js
@@ -7,14 +7,18 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // 設定本地登入策略
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', 
+  passReqToCallback: true}, 
+  (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          req.flash('warning_msg', 'Email 還未被註冊！')
+          return done(null, false)
         }
         if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
+          req.flash('warning_msg', 'Email 或 password 錯誤')
+          return done(null, false)
         }
         return done(null, user)
       })
