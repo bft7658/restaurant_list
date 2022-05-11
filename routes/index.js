9651b6a1efb8b68dbc603397c5d6aa56fda7b65c
bft7658 <bft7658@gmail.com>
2022-05-11 16:16:49 +0800

    feat: add user authentication in restaurants and home routers

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const home = require('./modules/home')
 // 引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+// 掛載 middleware
+const { authenticator } = require('../middleware/auth')  
 
 // 將網址結構符合的字串導向對應的模組 
-router.use('/' || '/search', home)
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
+router.use('/' || '/search', authenticator, home)
 
 module.exports = router
