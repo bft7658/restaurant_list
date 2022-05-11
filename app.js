55250203319b1c5adb3fad1dda68879b9b25489c
bft7658 <bft7658@gmail.com>
2022-05-11 14:21:14 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 // 引用 body-parser
 const bodyParser = require('body-parser')
+const session = require('express-session')
 // 載入 method-override
 const methodOverride = require('method-override')
 // 引用路由器
@@ -18,6 +19,12 @@ app.set('view engine', 'handlebars')
 
 // 設定靜態檔案
 app.use(express.static('public'))
+// 設定 express-session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
