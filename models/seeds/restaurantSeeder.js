b2e99afb8274dc069ef39350a2d86e1a271f785d
bft7658 <bft7658@gmail.com>
2022-05-12 22:09:51 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,12 +1,51 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../restaurant')
+const User = require('../user')
 // 載入 restaurant 的資料
 const restaurantList = require("../../restaurant.json").results
 const db = require('../../config/mongoose')
 
+// 建構測試使用者
+const SEED_USERS = [
+  {
+    name: 'user123',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurants: restaurantList.slice(0, 3)
+  },
+  {
+    name: 'user456',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurants: restaurantList.slice(3, 6)
+  }
+]
+
+
 db.once('open', () => {
-  Restaurant.create(restaurantList)
+  Promise.all(Array.from(SEED_USERS, seedUser => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash => User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        seedUser.restaurants.forEach(restaurant => {
+          restaurant.userId = userId
+        })
+        return Restaurant.create(seedUser.restaurants)
+      })
+  }))
     .then(() => {
-      console.log("done!")
+      console.log("Done!")
+      process.exit()
     })
     .catch(error => console.log(error))
 })
