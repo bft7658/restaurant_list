6c1c2efab0aad7a5f662f6846894fe536301dc3e
bft7658 <bft7658@gmail.com>
2022-05-13 16:02:44 +0800

    feat: add register successful message

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -65,7 +65,7 @@ router.post('/register', (req, res) => {
         email,
         password: hash // 用雜湊值取代原本的使用者密碼
       }))
-      .then(() => res.redirect('/'))
+      .then(() => res.render('login', { email }))
       .catch(err => console.log(err))
   })
 })
